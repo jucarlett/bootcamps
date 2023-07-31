05345e2db7a8532c957e8c39e711c18ec19c3f4e
Julie Carlett <95197835+jucarlett@users.noreply.github.com>
2023-07-31 19:50:48 -0300

    up generics

---
+++ todastech_dart/aula05/02generics.dart
@@ -0,0 +1,48 @@
+//T significa q é generico a classe de exibicao dos dados
+class Segmento<T> {
+  T? produto;
+  Segmento(produto) {
+    this.produto = produto;
+  }
+  void exibir() {
+    if (this.produto is Vestuario) {
+      print("Obj do tipo Vestuário");
+    }
+    if (this.produto is Smartphones) {
+      print("Obj do tipo Smartphones");
+    }
+  }
+}
+
+class Vestuario {
+  String? produto;
+  String? tamanho;
+
+  Vestuario(String produto, String tamanho) {
+    this.produto = produto;
+    this.tamanho = tamanho;
+  }
+}
+
+class Smartphones {
+  String? produto;
+  String? processador;
+  String? sistema;
+
+  Smartphones(String produto, String processador, String sistema) {
+    this.produto = produto;
+    this.processador = processador;
+    this.sistema = sistema;
+  }
+}
+
+void main() {
+  Vestuario v = new Vestuario("Camiseta", "M");
+  Smartphones s = new Smartphones("iPhone", "A14", "iOS");
+
+  Segmento<Vestuario> obj1 = new Segmento<Vestuario>(v);
+  obj1.exibir();
+
+  Segmento<Smartphones> obj2 = new Segmento<Smartphones>(s);
+  obj2.exibir();
+}
