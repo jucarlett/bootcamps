ccfae0190416b280766c6b68581b6b3f50ad189f
Julie Carlett <95197835+jucarlett@users.noreply.github.com>
2023-08-07 20:31:04 -0300

    update scaffold StatefulWidget  child container

---
+++ todastech_flutter/aula02/lib/Exemplo01.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Pagina extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ConteudoPagina();
+  }
+}
+
+//classe contendo toda interface gráfica
+class ConteudoPagina extends State {
+  //variavel
+  Color cor = Colors.purple;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          if (cor == Colors.purple) {
+            cor = Colors.green;
+          } else {
+            cor = Colors.purple;
+          }
+        });
+      },
+      child: Text('Clique aqui'),
+      style: ElevatedButton.styleFrom(backgroundColor: cor),
+    );
+  }
+}
---
+++ todastech_flutter/aula02/lib/Exemplo02.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class Pagina extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return ConteudoPagina();
+  }
+}
+
+//classe contendo toda interface gráfica
+class ConteudoPagina extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Utilizando scaffold')),
+        body: SizedBox(
+            child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.green)),
+              child: const Center(child: Text('1')),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(border: Border.all(color: Colors.red)),
+              child: Image.network(
+                  'https://cdn4.iconfinder.com/data/icons/logos-brands-5/24/flutter-256.png'),
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(border: Border.all(color: Colors.blue)),
+            ),
+          ],
+        )));
+  }
+}
