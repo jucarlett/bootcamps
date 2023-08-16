cfee305a3d8591cbe407aeb4d0c18caa47f9d8ad
Julie Carlett <95197835+jucarlett@users.noreply.github.com>
2023-08-16 20:20:20 -0300

    up cadastro e exclusão por elevatedbutton

--- todastech_flutter/aula05/lib/main.dart
+++ todastech_flutter/aula05/lib/main.dart
@@ -35,13 +35,118 @@ class Pagina extends StatefulWidget {
 
 // State
 class ConteudoPagina extends State {
+  // Lista de nomes
+  List<String> nomes = ["Ralf", "Carla", "Bianca", "Larissa", "Pâmela"];
+
+  // Variável para armazenar o nome digitador
+  String? nome;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("5ª aula de Flutter"),
       ),
-      body: const Text("Hello World!"),
+      body: SizedBox(
+        child: Column(
+          children: [
+            // Caixa de texto e Botão
+            SizedBox(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: TextField(
+                      onChanged: (valorDigitado) {
+                        setState(() {
+                          nome = valorDigitado;
+                        });
+                      },
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          nomes.add(nome!);
+                        });
+                      },
+                      child: const Text("Cadastrar"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            // ListView
+            SizedBox(
+              height: 300,
+              child: ListView.builder(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: nomes.length,
+                itemBuilder: (BuildContext obj, int indice) {
+                  return Card(
+                    color: Colors.green,
+                    child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(children: [
+                          Text(nomes[indice]),
+                          ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                nomes.removeAt(indice);
+                              });
+                            },
+                            child: const Text("Excluir"),
+                          ),
+                        ])),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
+
+
+/*
+Segundo Exemplo:
+
+ListView(
+        children: const [
+          Card(
+            color: Colors.green,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text("Linha 01"),
+            ),
+          ),
+        ],
+      )
+*/
+
+
+
+
+
+
+
+/*
+Primeiro Exemplo: 
+
+ListView(
+  children: const [
+    ListTile(
+      title: Text("Linha 01"),
+    ),
+    ListTile(
+      title: Text("Linha 02"),
+    ),
+  ],
+)
+*/
