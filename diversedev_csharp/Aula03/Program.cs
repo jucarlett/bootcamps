45d9c655aa11f31844185ea07be9a59652ceb78a
Julie Carlett <carlettju@gmail.com>
2023-10-06 22:03:18 -0300

    up

--- diversedev_csharp/Aula03/Program.cs
+++ diversedev_csharp/Aula03/Program.cs
@@ -44,6 +44,43 @@ Console.WriteLine($"Resultado: {resultado}");
 //3 validacao do dado
 //4 se dado invalido - imprimir mensagem de erro e voltar para o passo 1
 //5 se dado valido - continuar
+//while e do while
+
+
+
+//todos comando iguais de implementacao de lista
+// O array utiliza um espaço sequencial na memória, enquanto a lista trabalha "apontando" espaços adicionais, não precisa ser sequencial
+List<string> lista = new List<string>();
+List<string> lista_ = new();
+var list2 = new List<string>();
+
+lista_.Add("ex1");
+lista.Add("2");
+lista.Add("3");
+lista.Remove("2");
+lista.RemoveAt(0);
+lista.RemoveAll(x => x == "2"); //remove todos os elementos que atendem a condição "2"
+lista_.AddRange(lista);
+
+
+//arrays - sempre deve ser definido o tamanho,se quer algo dinamico usar LIST
+string[] array1 = { "0", "1", "2" }; //instancia os valores
+string[] array2 = new string[4]; //sem instanciar os valores
+int[] array3 = new int[4];
+var array4 = new int[4];
+
+
+array1.SetValue("5", 0); //altera o valor do indice 0 para 5
+array1[0] = "5"; //altera o valor do indice 0 para 5
+
+
+
+
+
+
+
+
+
 
 
 
